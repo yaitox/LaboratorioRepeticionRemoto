c3b19506392d2aa724113470c10e12b9f633e171
Rueda Romero <000402237@bga.upb>
2019-03-01 14:53:18 -0500

    ejercicio dos resuelto

--- LaboratorioRepeticion/src/par.py
+++ LaboratorioRepeticion/src/par.py
@@ -1,6 +1,8 @@
-x=int(input("digite un numero"))
-if x%2==0:
-    print("es par")
-
-else:
-    print("es impar")
+par=0
+for x in range(-1000000000000000000,1000000000000000):
+    x=int(input("ingrese los numeros"))
+    if x%2==0:
+        x=x+1
+    else:
+        break
+        print("hay ",par," numeros positivos")
